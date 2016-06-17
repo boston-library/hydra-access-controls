18ae6a34dfd37dc4f68b01c649bea5ae7b1a2530
Steven Anderson <sanderson@bpl.org>
2016-06-17 15:54:14 -0400

    first commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,44 @@
+ENV["environment"] ||= "test"
+
+require 'rspec/mocks'
+require 'rspec/its'
+require 'hydra-access-controls'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+Hydra::Engine.config.autoload_paths.each { |path| $LOAD_PATH.unshift path }
+
+if ENV['COVERAGE'] and RUBY_VERSION =~ /^1.9/
+  require 'simplecov'
+  require 'simplecov-rcov'
+
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  SimpleCov.start
+end
+
+
+require 'support/rails'
+
+# Since we're not doing a Rails Engine test, we have to load these classes manually:
+require_relative '../app/models/role_mapper'
+require_relative '../app/models/ability'
+require_relative '../app/services/hydra/lease_service'
+require_relative '../app/services/hydra/embargo_service'
+require_relative '../app/validators/hydra/future_date_validator'
+require 'support/mods_asset'
+require 'support/solr_document'
+require "support/user"
+require "factory_girl"
+require "factories"
+
+
+RSpec.configure do |config|
+
+end
+
+# Stubbing Devise
+class Devise
+  def self.authentication_keys
+    ["uid"]
+  end
+end
